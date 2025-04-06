f8d7ecce73ab1ea9ec7de3f9d14931bec01e1156
kylev <kylev2005@hotmail.com>
2025-04-07 00:56:35 +0100

    spring boot API

---
+++ inv-manager/user-interface/src/app/components/data-viewer/data-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-viewer',
+  imports: [],
+  templateUrl: './data-viewer.component.html',
+  styleUrl: './data-viewer.component.css'
+})
+export class DataViewerComponent {
+
+}
---
+++ springboot/src/test/java/io/inventory_manager/InventoryAPI/InventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.inventory_manager.InventoryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
