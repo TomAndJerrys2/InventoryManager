d3804069b3a7889e8e579f79490d12de898e15ca
kylev <kylev2005@hotmail.com>
2025-04-07 05:05:50 +0100

    navbar added

---
+++ inv-manager/user-interface/src/app/components/stock-manager/stock-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-manager',
+  imports: [],
+  templateUrl: './stock-manager.component.html',
+  styleUrl: './stock-manager.component.css'
+})
+export class StockManagerComponent {
+
+}
